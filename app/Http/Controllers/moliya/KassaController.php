a06a91143573dbd54ac795232f615d30346d68b2
USER <elshodatc111>
2025-06-27 15:19:18 +0500

    Save

--- app/Http/Controllers/moliya/KassaController.php
+++ app/Http/Controllers/moliya/KassaController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\moliya;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Balans;
+use App\Models\BalansHistory;
+use App\Models\Kassa;
+use Carbon\Carbon;
 
 class KassaController extends Controller
 {
     public function index(){
-        return view('moliya.kassa');
+        $Kassa = Kassa::first();
+        return view('moliya.kassa',compact('Kassa'));
     }
 }
